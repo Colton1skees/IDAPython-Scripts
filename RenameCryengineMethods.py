33c6a700c08250bf2d2158148a3cced4954e639d
Connor Skees <39542938+ConnorSkees@users.noreply.github.com>
2019-05-30 23:22:19 -0400

    Refactor
    This pull request
    - Revises module-level docstring
    - Removes unnecessary nesting
    - Removes unused imports and wildcard import
    - Uses regular expressions to more reliably parse strings
    - Decreases line lengths to be under 90
    - Renames constant variables to be UPPER_CASE

--- RenameCryengineMethods.py
+++ RenameCryengineMethods.py
@@ -1,36 +1,39 @@
-# This script is designed to automatically reverse cryengine functions with left over debug strings.
-# It parses every string for the pattern ClassName::MethodName and automatically renames the method to it's proper name. (This is accurate in 99% of cases)
-# Finally, it dumps the address + actual name of the function to a .txt, which is useful in other programs.
-# I personally used the .txt file for my Reclass.NET plugin, which parses the txt file and automatically renames/identifies applicable Vtable methods and classes. This also makes code generation very clean/
+"""
+This script automatically reverses cryengine functions with leftover debug
+strings.
 
+It parses every string for the pattern ClassName::MethodName and automatically
+renames the method to its proper name. It dumps the address + actual name of the
+function to a .txt file.
+"""
+import re
+
+import idaapi
 import idautils
-import idc
-from idaapi import *
-strings = idautils.Strings()
-invalidStrings = [",", "/", "\\", ".", "(", ")", "-", "<", ">", " "]
 
 
-for string in strings:
+STRINGS = idautils.Strings()
+
+for string in STRINGS:
     programString = str(string)
-    if '::' in programString:
-        programString = programString.split(" ", 1)[0]
-        programString = programString.replace("~", "_")
-        for invalidString in invalidStrings:
-            programString = programString.translate(None, invalidString) 
-        if programString.count("[") == 1 and programString.count("]") == 1:
-            programString = programString[programString.find("[")+1:programString.find("]")]
-        xrefs = idautils.XrefsTo(string.ea)
-        count = sum(1 for _ in xrefs)
-        if count == 1:
-            xrefsNew = idautils.XrefsTo(string.ea)
-            for xref in xrefsNew:
-                    func = idaapi.get_func(xref.frm)
-                    name = get_func_name(xref.frm)
-                    if 'None' not in str(name):
-                        print("RandomName: {}, ActualName: {}".format(name, programString))
-                        address = (GetFunctionAttr(xref.frm, FUNCATTR_START))
-                        print(address)
-                       # idc.MakeNameEx(address, programString, 0x00)
-                        
-    
-    
+    if '::' not in programString:
+        continue
+    programString = programString.replace("~", "_")
+    programString = re.search(r"[^ :\[<]+::[^ :>\]]+", programString)
+    if programString:
+        programString = programString.group(0)
+    else:
+        continue
+    xrefs = idautils.XrefsTo(string.ea)
+    # count elements in generator
+    if sum(1 for _ in xrefs) != 1:
+        continue
+    xrefsNew = idautils.XrefsTo(string.ea)
+    for xref in xrefsNew:
+        func = idaapi.get_func(xref.frm)
+        name = idaapi.get_func_name(xref.frm)
+        if name is not None:
+            print("RandomName: {}, ActualName: {}".format(name, programString))
+            address = idaapi.GetFunctionAttr(xref.frm, idaapi.FUNCATTR_START)
+            print(address)
+            # idc.MakeNameEx(address, programString, 0x00)
